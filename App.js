c30fccd638a938b2dc3f9ff53e2f46cbc480bead
Paige Gorry <paigeegorry@gmail.com>
2020-01-17 11:40:51 -0800

    setting up first react native app, following along udemy tutorial linked in README.md

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  const [outputText, setOutputText] = useState('')
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+      <Button title="change text" onPress={() => setOutputText('The text changed')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# react native app playground
+
+Following along [Udemy course](https://www.udemy.com/course/react-native-the-practical-guide/)
